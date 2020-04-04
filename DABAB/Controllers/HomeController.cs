c72c58f8853cd9d2c08da8dfd9b9d05f96d6d773
Borna Hrastovec <bornahrastovec@outlook.com>
2020-04-04 17:40:22 +0200

    HomeController merge change

--- DABAB/Controllers/HomeController.cs
+++ DABAB/Controllers/HomeController.cs
@@ -1,23 +1,23 @@
-﻿using DABAB.Models;
-using DABAB.Models.Helpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DABAB.Models;
+using DABAB.Models.Helpers;
 using System.Web.Mvc;
 
 namespace DABAB.Controllers
 {
+
     public class HomeController : Controller
     {
+
         private readonly BazaDbContext context = new BazaDbContext();
-        private readonly QueryHelper queryHelper = new QueryHelper();
+        private readonly QueryHelper repository = new QueryHelper();
 
         public ActionResult Index()
         {
-
-
-            return View(context.Filmovi.ToList());
+            return View();
         }
 
         public ActionResult About()
